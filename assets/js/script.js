5186b741124e3d868718a31a41a3a523cb8aac3e
gc796 <collares.gonzalo@gmail.com>
2023-05-09 20:17:31 -0300

    Home page: Peli principal traida de la API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const id = "500";
 const API = `https://api.themoviedb.org/3`;
-const imgPath =  `https://image.tmdb.org/t/p/w185/`;
+const imgCard =  `https://image.tmdb.org/t/p/w185/`;
 const backImg = "https://image.tmdb.org/t/p/original/"
 
 const options = {
@@ -11,53 +11,67 @@ const options = {
     }
   };
 
-
-
 // Fetch de popular movies
 fetch(`${API}/movie/popular?language=en-US&page=1`, options)
   .then(response => response.json())
   .then(response => {
     console.log("popular movies", response)
     popularMoviesGrid(response.results);
-    principalMovie(response.results[0])
   })
   .catch(err => console.error(err));
 
 
-  function principalMovie(movie){
+  function firstMovie(movie){
 
+    // Select parent element
     let section = document.getElementById("principal-movie");
+    // Creates child element
     let div = document.createElement("div");
+
+    //Movie background
+    section.style.backgroundImage = `radial-gradient(circle, rgba(255,255,255,0) 0%, rgba(0,0,0,1) 80%),url(${backImg}${movie.backdrop_path})`;
+    // Add class to child element 
     div.classList.add("container-principal-movie");
-    
-    //Probar estilo en linea para el background dinamico y con color negro encima
+  
+    // Html inyection in parent element
     div.innerHTML = `
         <h2>${movie.title}</h2>
-        <img class="principal-movie" src="${backImg}${movie.backdrop_path}">  
-        <div class="principal-movie__info">
+        <div>
            <p>${movie.overview}</p>
+           <button>More info..</button>
         </div>
+        
     `
     section.appendChild(div);
 
   }
 
   function popularMoviesGrid(data){
-
+    let cont = 0;
     data.forEach(movie => {
-     /*selecciona el elementro padre*/
-      let parent = document.getElementById("popular-movies");
-      /*crea un elemento a indexar o inyectar*/
-      let div = document.createElement("div");
-      div.classList.add("card");
-      /*le agreo lo que inyecto*/
-      div.innerHTML = `
-      <div>
-      <img src="${imgPath}${movie.poster_path}" alt="${movie.title}" />
-      </div>
-      `;
-      /* agrego elemento hijo  al padre*/
-      parent.appendChild(div);
+      
+      if(cont == 0) {
+
+       firstMovie(movie);
+
+      } else {
+         // Select parent element
+         let parent = document.getElementById("popular-movies");
+        // Creates child element
+         let div = document.createElement("div");
+         // Add class to child element 
+         div.classList.add("card");
+         // Html inyection in parent element
+         div.innerHTML = `
+         <div>
+         <img src="${imgCard}${movie.poster_path}" alt="${movie.title}" />
+         </div>
+         `;
+         /* agrego elemento hijo  al padre*/
+         parent.appendChild(div);
+         
+        }
+        cont++;
     });
 }
 
