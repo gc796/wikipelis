d19ec46c169e45217ace629318a8c5ef2eb1e34f
Alejo Medina <alejorc95@icloud.com>
2023-05-12 20:54:57 -0300

    new releases commit

---
+++ assets/js/movie.js
@@ -0,0 +1,25 @@
+// //-----------------SCROLL PROGRESS BAR----------------------//
+
+// let scrollPercentage = () => {
+//     let scrollPercentage = document.getElementById("progress");
+//     let progressValue = document.getElementById("progress-value");
+//     let pos = document.documentElement.scrollTop;
+//     let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//     let scrollValue = Math.round(pos * 100 / calcHeight);
+//     scrollProgress.style.background = `conic-gradient(#e70634 ${scrollValue})%, #2b2f38 ${scrollValue}%)`;
+// }
+
+// window.onscroll = scrollPercentage;
+// window.onload = scrollPercentage;
+
+
+// // ----------------FIXED NAVIGATION BUTTON---------------------//
+
+// $(window).scroll(function() {
+//     if($(document).scrollTop() > 20) {
+//         $('.navigation').addClass('fix-icon');
+//     }
+//     else{
+//         $('.navigation').removeClass('fix-icon');
+//     }
+// });
